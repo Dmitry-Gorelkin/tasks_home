363d615ea8af3cb57e4738a5a735ad56f0aaba0e
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2021-12-20 23:59:01 +0200

    + что-то

--- js/solutions.js
+++ js/solutions.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// function sendEmailList() {
+//   for (let i = 1; i <= 30; i++) {
+//     console.log("Gromcode party invitation was sent!");
+//   }
+// }
+
+// sendEmailList();
+
+function sendEmail() {
+  return "Gromcode party invitation was sent!";
+}
+
+function sendEmailList() {
+  for (let i = 0; i < 30; i++) {
+    console.log(sendEmail());
+  }
+}
+sendEmailList();
